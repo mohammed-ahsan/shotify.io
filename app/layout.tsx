5d69c684e0d002d369dc4cb0f26203c311b7d8fd
Mohammed Ahsan <earth.ahsan@gmail.com>
2024-02-27 06:34:22 +0600

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const IBMPlex = IBM_Plex_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "Imaginify",
+  title: "Shotify.io",
   description: "AI-powered image generator",
 };
 
--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="wdnyhnud"
       options={{
         multiple: false,
         resourceType: "image",
